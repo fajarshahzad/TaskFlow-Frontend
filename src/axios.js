6f47cd9ac8735dc25d95fee67808937ef2fba2e5
fajarshahzad <ahsanshahzad112345@gmail.com>
2026-01-05 19:47:48 +0500

    Frontend

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api", // base backend URL
+  baseURL: "https://task-flow-backend-ashen.vercel.app/", // base backend URL
 });
 
 // Add token to every request
